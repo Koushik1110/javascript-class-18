89e53be217d6d98f51d88a7c06a72282180fc57c
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2023-11-30 21:48:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//object
+//object is also an in built data structure. Used in as a key-value pair.
+
+const student = {
+  fullName: "Rahul",
+  birthYear: 2003,
+  city: "Dhaka",
+  isDeveloper: true,
+  favoriteSubject: ["English", "JavaScript", "Python"]
+};
